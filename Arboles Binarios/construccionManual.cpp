b65c5068b1711ee5c0a85f780e363e8626b1d811
Irving2355 <it235597@gmail.com>
2026-08-17 18:45:06 -0600

    Clase Construccion manual de arboles

---
+++ Arboles Binarios/construccionManual.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Nodo{
+    char dato;
+    Nodo* izquierdo;
+    Nodo* derecho;
+};
+
+Nodo* crearNodo(char dato){
+    Nodo* nuevo = new Nodo;
+
+    nuevo->dato = dato;
+
+    nuevo->izquierdo = nullptr;
+    nuevo->derecho = nullptr;
+
+    return nuevo;
+}
+
+int main(){
+    Nodo* raiz = crearNodo('A');
+
+    raiz->izquierdo = crearNodo('B');
+    raiz->derecho = crearNodo('C');
+
+    raiz->izquierdo->izquierdo = crearNodo('D');
+    raiz->derecho->derecho = crearNodo('E');
+    return 0;
+}
