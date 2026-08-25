92d0cca9bf89cd32e0a83bb64bac1cda9f5ea231
Irving2355 <it235597@gmail.com>
2026-08-24 18:52:56 -0600

    Continuacion de ejerciosio practico

--- Arboles Binarios/ejercicioIntegrador.cpp
+++ Arboles Binarios/ejercicioIntegrador.cpp
@@ -21,6 +21,96 @@ Nodo* crearNodo(string dato){
     return nuevo;
 }
 
+bool esOperador(const string& dato){
+    return dato == "+" ||
+           dato == "-" ||
+           dato == "*" ||
+           dato == "/";
+}
+
+//preorden generar notacion prefija
+/*
+(8+4)*(10-6)
+*,+,8,4,-,10,6
+*/
+void generarPrefija(
+    Nodo* nodo,
+    vector<string>& expresion
+){
+    if(nodo == nullptr){
+        return;
+    }
+
+    expresion.push_back(nodo->dato);
+
+    generarPrefija(
+        nodo->izquierdo,
+        expresion
+    );
+
+    generarPrefija(
+        nodo->derecho,
+        expresion
+    );
+}
+
+/*
+Inorden
+izq raiz der
+8,+,4,*,10,-,6
+*/
+
+string generarInfija(Nodo* nodo){
+    if(nodo == nullptr){
+        return "";
+    }
+
+    //si es una hoja 
+    if(
+        nodo->izquierdo == nullptr &&
+        nodo->derecho == nullptr
+    ){
+        return nodo->dato;
+    }
+
+    string izquierda = generarInfija(nodo->izquierdo);
+    string derecha = generarInfija(nodo->derecho);
+
+    return "(" +
+        izquierda +
+        " " +
+        nodo->dato +
+        " " +
+        derecha +
+        ")";
+}
+
+/*
+Postorden
+izq der raiz
+8,4,+,10,6,-,*
+*/
+void generarPostfija(
+    Nodo* nodo,
+    vector<string>& expresion
+){
+    if(nodo == nullptr){
+        return;
+    }
+
+    generarPostfija(
+        nodo->izquierdo,
+        expresion
+    );
+
+    generarPostfija(
+        nodo->derecho,
+        expresion
+    );
+
+    expresion.push_back(nodo->dato);
+}
+
 int main(){
     return 0;
 }
